93a23439ae11fc4aa0eec669f5ce2327c4d875be
Alex Rowan <alex@onboardhq.com>
2018-02-05 15:36:38 +0000

    refactor Tax, now uses class methods

--- lib/calculator/tax.rb
+++ lib/calculator/tax.rb
@@ -1,21 +1,19 @@
 module Calculator
   class Tax
-    attr_reader :income
 
-    def initialize(income)
-      @income = income
-      @tax_brackets = [
-        Bracket.new(nil, 11_500, 0),
-        Bracket.new(11_501, 45_000, 20),
-        Bracket.new(45_001, 150_000, 40),
-        Bracket.new(150_000, nil, 45)
-      ]
-    end
+    BRACKETS ||= [
+      Bracket.new(nil, 11_500, 0),
+      Bracket.new(11_501, 45_000, 20),
+      Bracket.new(45_001, 150_000, 40),
+      Bracket.new(150_000, nil, 45)
+    ]
 
-    def tax_rate
-      @tax_brackets.each do |tax_bracket|
-        if tax_bracket.in_bracket?(@income)
-          return tax_bracket
+    class << self
+      def find_bracket(income)
+        BRACKETS.each do |tax_bracket|
+          if tax_bracket.in_bracket?(income)
+            return tax_bracket
+          end
         end
       end
     end
